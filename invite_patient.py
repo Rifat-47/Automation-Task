251db78482f79c514757ad634698a42ceb8a19f4
rifat <rifatscc1747@gmail.com>
2023-12-30 02:08:19 +0600

    inivitation to patient updated

--- invite_patient.py
+++ invite_patient.py
@@ -7,20 +7,21 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 
 website = "https://stg.injurycloud.com"
 driver.get(website)
-time.sleep(5)
+# time.sleep(5)
 driver.maximize_window()
+time.sleep(2)
 
 driver.find_element(by="xpath", value="//input[@id='Email']").send_keys('rtaher@mymedicalhub.com')
-time.sleep(3)
+time.sleep(2)
 
 driver.find_element(by="xpath", value="//input[@id='Password']").send_keys('QWEqwe1234@')
-time.sleep(3)
+time.sleep(2)
 
 driver.find_element(by="xpath", value="//button[@id='login-btn']").click()
 time.sleep(7)
 
 driver.find_element(by="xpath", value="//a[normalize-space()='Invite a Patient']").click()
-time.sleep(3)
+time.sleep(2)
 
 driver.find_element(by="xpath", value="//label[normalize-space()='Self Assessment']").click()
 time.sleep(2)
@@ -29,13 +30,23 @@ driver.find_element(by="xpath", value="//div[@id='msk-region']//div[5]").click()
 time.sleep(2)
 
 email = driver.find_element(by="xpath", value="//input[@id='emails']")
-email.send_keys('vatbwebtest1@yopmail.com')
+email.send_keys('stgwebtest103@yopmail.com')
 driver.execute_script("arguments[0].blur();", email)
-time.sleep(5)
+time.sleep(4)
 
-# driver.find_element(by="xpath", value="//input[@id='FirstName_R']").send_keys('stgweb')
+firstName = driver.find_element(by='xpath', value="//input[@id='FirstName_R']")
+print(firstName.get_attribute('value'))
+if not firstName.get_attribute('value'):
+    firstName.send_keys('stgweb')
+    time.sleep(2)
+# driver.find_element(by="xpath", value="//input[@id='FirstName_R']")
 # time.sleep(3)
 
+lastName = driver.find_element(by="xpath", value="//input[@id='LastName_R']")
+print(lastName.get_attribute('value'))
+if not lastName.get_attribute('value'):
+    lastName.send_keys('test1')
+    time.sleep(2)
 # driver.find_element(by="xpath", value="//input[@id='LastName_R']").send_keys('test1')
 # time.sleep(3)
 
